ac85f78bfe3ad132d9ed0c7f2cde8ea9aaaebd57
“oleharkhipkov” <oleharkhipkov0613@gmail.com>
2021-04-12 15:31:04 -0500

    add dirname var

--- server/app.js
+++ server/app.js
@@ -60,12 +60,17 @@ app.use('/conversations', conversationRouter);
 app.use('/messages', messageRouter);
 app.use('/users', userRouter);
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
   );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 app.use(notFound);
