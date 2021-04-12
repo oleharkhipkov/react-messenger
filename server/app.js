18daeeee8f8ba5a0a590f2e0b6d4d1eb4c9d771b
“oleharkhipkov” <oleharkhipkov0613@gmail.com>
2021-04-12 16:04:02 -0500

    set build directory as static asset

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ app.use('/messages', messageRouter);
 app.use('/users', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html')
